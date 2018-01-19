9f394502b6b5d3561b12950556a1c56f15673aa2
Joe Logue <jlogue908@gmail.com>
2018-01-19 16:51:56 -0500

    tottaly

--- app/friends.js
+++ app/friends.js
@@ -1,53 +1,68 @@
-var friends = [
-{
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-},
-{
-  "name":"Steve",
-  "photo":"https://www.designzzz.com/wp-content/uploads/2012/12/freestock_image_by_goldman555-d495w4s.jpg",
-  "scores":[
-      2,
-      1,
-      3,
-      4,
-      1,
-      1,
-      2,
-      5,
-      2,
-      3
-    ]
-},
-{
-  "name":"Mike and Phil",
-  "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr05/14/8/50-completely-unexplainable-stock-photos-no-one-w-2-9544-1400070187-18_dblbig.jpg",
-  "scores":[
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      2,
-      1,
-      1,
-      1
-    ]
-},
+var friends = [{
+        "name": "Ahmed",
+        "photo": "http://draxproduktion.com/wp-content/uploads/2013/01/Gente-7-e1365609907692.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Steve",
+        "photo": "https://www.designzzz.com/wp-content/uploads/2012/12/freestock_image_by_goldman555-d495w4s.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            4,
+            3,
+            3,
+            2,
+            5,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Mike and Phil",
+        "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2014-05/campaign_images/webdr05/14/8/50-completely-unexplainable-stock-photos-no-one-w-2-9544-1400070187-18_dblbig.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            2,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Hannah",
+        "photo": "https://static.pexels.com/photos/372042/pexels-photo-372042.jpeg",
+        "scores": [
+            2,
+            2,
+            2,
+            3,
+            2,
+            3,
+            2,
+            2,
+            1,
+            2
+        ]
+    },
 
 ]
 
-module.exports = friends ;
+module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,23 +1,23 @@
 var path = require("path");
 var friends = require("../app/friends.js");
 
-function api (app){
-app.post("/api/new", function(req, res) {
-    var newFriend = req.body;
+function api(app) {
+    app.post("/api/new", function(req, res) {
+        var newFriend = req.body;
 
-    console.log(newFriend);
+        console.log(newFriend);
 
-    friends.push(newFriend);
+        friends.push(newFriend);
 
-    res.json(newFriend);
-})
+        res.json(newFriend);
+    })
 
-app.get("/api/all", function(req, res) {
-    res.json(friends);
-})
+    app.get("/api/all", function(req, res) {
+        res.json(friends);
+    })
 
 }
 
 
 
-module.exports = api ;
+module.exports = api;
